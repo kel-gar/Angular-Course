034fb3e9b1ae122000613a6ce7a7af34e1b83b84
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2019-09-29 21:25:11 -0600

    component section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,20 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  courses = COURSES;
 
+  title = COURSES[0].description;
+  
+  price = 9.991234;
+
+  rate= 0.67
+
+  startDate = new Date(2000, 0, 1);
+
+  onCourseSelected(course:Course) {
+
+    console.log("APP COMPONENT - CLICKED", course);
+  }
+ 
 
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import {COURSES} from '../../db-data';
+import {Course} from '../model/course';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'course-card',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
+ 
+  @Input()
+  course:Course;
+
+  @Input()
+  cardIndex:number;
+ 
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }  
+
+  onCourseViewed() {
+    console.log("CARD COMPONENT CLICKED");
+
+    this.courseEmitter.emit(this.course);
+  }
+
+  cardClasses(){
+    if(this.course.category == "BEGINNER"){
+      return ['beginner'];
+    }
+  } 
+
+  cardStyles(){
+    return {
+      'background-image': 'url(' + this.course.iconUrl + ')'
+    };
+  }
 }
